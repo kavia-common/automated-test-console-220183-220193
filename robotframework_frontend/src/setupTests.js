d50f84ef581db0b3423c1cfe78d07bfdf46bde19
Kavia AI <production@kavia.ai>
2025-12-05 02:44:04 +0000

    Checkpoint 12 - cge36bfd66

--- robotframework_frontend/src/setupTests.js
+++ robotframework_frontend/src/setupTests.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// Ensure jest-dom is loaded for all test suites
 import '@testing-library/jest-dom';
